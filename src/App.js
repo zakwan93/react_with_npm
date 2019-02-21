4996d90038604efe12e4bf670a979bf6933e143f
zakwan93 <zakwan_bhaiyat@yahoo.com>
2019-02-20 23:45:25 -0500

    Understand the use of STATE in React and how to declare data in state and access data from state

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Zakwan', age: 25},
+      {name: 'Shehjad', age: 26},
+      {name: 'Yahya', age: 27}
+    ]
+  }
   render() {
     return (
       <div className="App">
        <h1> Hi, I'm a React App </h1>
        <p> This is awesome </p>
-       <Person name = "Zakwan" age="25"> I'm getting better at React </Person>
-       <Person name = "Shehjad" age = "26" />
-       <Person name = "Yahya" age = "27" />
+       <Person name = {this.state.persons[0].name} age={this.state.persons[0].age}> I'm getting better at React </Person>
+       <Person name = {this.state.persons[1].name} age ={this.state.persons[1].age} />
+       <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div',{className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
