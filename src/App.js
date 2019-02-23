c1dce66d4b4024914bb74f20a1c7cb05cef51ed9
zakwan93 <zakwan_bhaiyat@yahoo.com>
2019-02-22 22:47:28 -0500

    learn how to add inline style and add style usinng stylesheets and import it into your component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-
 class App extends Component {
   state = {
     persons: [
@@ -34,11 +33,22 @@ class App extends Component {
   }
 
   render() {
+    const buttonStyle = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
        <h1> Hi, I'm a React App </h1>
        <p> This is awesome </p>
-       <button onClick = {() => this.mySwitchHandler('Zakwan!!!')}> My Switch! </button>
+       <button 
+          style = {buttonStyle}
+          onClick = {() => this.mySwitchHandler('Zakwan!!!')}> My Switch! 
+       </button>
        <Person 
           name = {this.state.persons[0].name} 
           age={this.state.persons[0].age} 
