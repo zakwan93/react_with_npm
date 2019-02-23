a3514cf0ae649bbeb431b7f08f321f116203abb2
zakwan93 <zakwan_bhaiyat@yahoo.com>
2019-02-22 22:15:16 -0500

    learn about passisng refrennces between components using bind and es6 return syntax

--- src/App.js
+++ src/App.js
@@ -1,57 +1,69 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const app = props => {
-  const [personsState, setPersonState] = useState({
+
+class App extends Component {
+  state = {
     persons: [
       {name: 'Zakwan', age: 25},
       {name: 'Shehjad', age: 26},
       {name: 'Yahya', age: 27}
     ]
-  });
+  }
 
-  const [otherState, setOtherState] = useState('Some other value');
-
-  const mySwitchHandler = () => {
+  mySwitchHandler = (newName) => {
     // console.log("Was Clicked!");
-    setPersonState ({
+    this.setState ({
       persons: [
-        {name: 'Zakwan Bhaiyat', age: 25},
+        {name: newName, age: 25},
         {name: 'Shehjad Bhaiyat', age: 26},
         {name: 'Yahya Bhaiyat', age: 27}
       ]
     })
   }
 
-  return (
-    <div className="App">
-      <h1> Hi, I'm a React App </h1>
-      <p> This is awesome </p>
-      <button onClick = {mySwitchHandler}> My Switch! </button>
-      <Person name = {personsState.persons[1].name} age={personsState.persons[1].age}> I'm getting better at React </Person>
-      <Person name = {personsState.persons[0].name} age ={personsState.persons[0].age} />
-      <Person name = {personsState.persons[2].name} age = {personsState.persons[2].age} > I'm learning !</Person>
-    </div>
-  );
-  // return React.createElement('div',{className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+  render() {
+    return (
+      <div className="App">
+       <h1> Hi, I'm a React App </h1>
+       <p> This is awesome </p>
+       <button onClick = {() => this.mySwitchHandler('Zakwan!!!')}> My Switch! </button>
+       <Person 
+          name = {this.state.persons[0].name} 
+          age={this.state.persons[0].age} 
+          click={this.mySwitchHandler.bind(this, 'Zakku!!')}>I'm getting better at React </Person>
+       <Person 
+          name = {this.state.persons[1].name} 
+          age ={this.state.persons[1].age} />
+       <Person 
+          name = {this.state.persons[2].name} 
+          age = {this.state.persons[2].age} > 
+          I'm learning !</Person>
+      </div>
+    );
+    // return React.createElement('div',{className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+  }
 }
 
-export default app;
+
+export default App;
 
 
-// class App extends Component {
-//   state = {
+// const app = props => {
+//   const [personsState, setPersonState] = useState({
 //     persons: [
 //       {name: 'Zakwan', age: 25},
 //       {name: 'Shehjad', age: 26},
 //       {name: 'Yahya', age: 27}
 //     ]
-//   }
+//   });
 
-//   mySwitchHandler = () => {
+//   const [otherState, setOtherState] = useState('Some other value');
+
+//   const mySwitchHandler = () => {
 //     // console.log("Was Clicked!");
-//     this.setState ({
+//     setPersonState ({
 //       persons: [
 //         {name: 'Zakwan Bhaiyat', age: 25},
 //         {name: 'Shehjad Bhaiyat', age: 26},
@@ -60,17 +72,14 @@ export default app;
 //     })
 //   }
 
-//   render() {
-//     return (
-//       <div className="App">
-//        <h1> Hi, I'm a React App </h1>
-//        <p> This is awesome </p>
-//        <button onClick = {this.mySwitchHandler}> My Switch! </button>
-//        <Person name = {this.state.persons[1].name} age={this.state.persons[1].age}> I'm getting better at React </Person>
-//        <Person name = {this.state.persons[0].name} age ={this.state.persons[0].age} />
-//        <Person name = {this.state.persons[2].name} age = {this.state.persons[2].age} > I'm learning !</Person>
-//       </div>
-//     );
-//     // return React.createElement('div',{className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
-//   }
+//   return (
+//     <div className="App">
+//       <h1> Hi, I'm a React App </h1>
+//       <p> This is awesome </p>
+//       <button onClick = {mySwitchHandler}> My Switch! </button>
+//       <Person name = {personsState.persons[1].name} age={personsState.persons[1].age}> I'm getting better at React </Person>
+//       <Person name = {personsState.persons[0].name} age ={personsState.persons[0].age} />
+//       <Person name = {personsState.persons[2].name} age = {personsState.persons[2].age} > I'm learning !</Person>
+//     </div>
+//   );
 // }
