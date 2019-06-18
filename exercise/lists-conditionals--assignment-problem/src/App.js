18e820b2be59849f1095fc8a832475607a840cc0
zakwan93 <zakwan_bhaiyat@yahoo.com>
2019-06-17 23:46:46 -0400

    Completed the list-conditionals assignment problem

--- exercise/lists-conditionals--assignment-problem/src/App.js
+++ exercise/lists-conditionals--assignment-problem/src/App.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Validation from './Validation/Validation';
+import Char from './Char/Char';
+
 class App extends Component {
+
+    state = {
+      userInput: ''
+    }
+
+    inputChangedHandler = (event) =>{
+        this.setState({userInput: event.target.value});
+    }
+
+    deleteCharHandler = (index) => {
+        const text = this.state.userInput.split('');
+        text.splice(index, 1);
+        const updatedText = text.join('');
+        this.setState({userInput: updatedText});
+    }
+
   render() {
+    const charList = this.state.userInput.split('').map((char, index) => {
+        return <Char character = {char} 
+                     key = {index}
+                     clicked = { () => this.deleteCharHandler(index)} />; //Passing a refrence syntax
+    });
+
     return (
       <div className="App">
         <ol>
@@ -14,6 +39,16 @@ class App extends Component {
           <li>When you click a CharComponent, it should be removed from the entered text.</li>
         </ol>
         <p>Hint: Keep in mind that JavaScript strings are basically arrays!</p>
+        <hr />
+
+
+         <input type="text" 
+         onChange={this.inputChangedHandler} 
+         value={this.state.userInput} />
+         <p> {this.state.userInput} </p>
+         <Validation inputLength = {this.state.userInput.length} />
+         {charList}
+
       </div>
     );
   }
---
+++ exercise/lists-conditionals--assignment-problem/src/Validation/Validation.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Validation';
+
+const validation  = (props) => {
+    let validationMessage = "Text long enough!";
+
+    if (props.inputLength <= 5){
+        validationMessage = "Text too short";
+    }
+
+    return(
+        <div>
+            <p> {validationMessage} </p>
+        </div>
+    );
+};
+
+
+export default validation;
